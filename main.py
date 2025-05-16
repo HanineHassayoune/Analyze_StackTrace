0fb8430ba78a00d318a5233e9479f912801647e6
HanineHassayoune <hassayounehanine@gmail.com>
2025-05-16 15:39:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()  # charge les variables d'environnement depuis le fichier .env
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+app = FastAPI()
+
+class LogInput(BaseModel):
+    stack_trace: str
+
+@app.post("/analyze")
+async def analyze_log(log: LogInput):
+    prompt = f"""
+Tu es un assistant expert en analyse de logs.
+Voici une stacktrace : {log.stack_trace}
+Catégorise précisément cette erreur selon son type exact, sans te limiter à une liste prédéfinie.
+Retourne uniquement la catégorie sous forme d'un texte court, par exemple 'Data Not Found Exception', 'Null Pointer Exception', 'Timeout Error', ou toute autre catégorie pertinente.
+"""
+
+
+    try:
+        response = model.generate_content(prompt)
+        category = response.text.strip()
+
+        return {
+            "category": category,
+            "original": log.stack_trace
+        }
+    except Exception as e:
+        return {"error": str(e)}
